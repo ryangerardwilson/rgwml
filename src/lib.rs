30f8decf9eef15231da89956954a9776f48e11e9
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2023-12-26 12:33:12 +0530

    sync

--- src/lib.rs
+++ src/lib.rs
@@ -87,7 +87,7 @@
 //!
 //!         dbg!(fuzzai_result);
 //!     }
-//!
+//! ```
 //! ## api_utils
 //!
 //! Easily print example synatax relating to this feature in your workflow.
@@ -104,6 +104,85 @@
 //!     }
 //! ```
 //!
+//! ## loop_utils
+//!
+//! ### FutureLoop
+//!
+//! `FutureLoop` provides a fluent interface to do multiple things at once (asynchronously) when dealing with a list or collection of items.
+//!
+//! The future_for function suits scenarios where you need a moderate level of concurrency. Since all futures are collected and then awaited together, it's important to ensure that spawning too many futures at once doesn't overwhelm the system resources. For instance, when each item's processing is relatively straightforward and doesn't require complex shared state or high-throughput parallelism.
+//!
+//! Use in a single-threaded Tokio environment
+//!
+//! ```
+//!     use rgwml::loop_utils::FutureLoop;
+//!
+//!     #[tokio::main()]async fn main() {
+//!         let data = vec![1, 2, 3, 4, 5];
+//!         let results = FutureLoop::future_for(data, |num| async move {
+//!         // Perform an async operation
+//!         num * 2
+//!         }).await;
+//!         println!("Results: {:?}", results);
+//!     }
+//! ```
+//! Use in a multi-threaded Tokio environment:
+//! ```
+//!     use rgwml::loop_utils::FutureLoop;
+//!
+//!     #[tokio::main(flavor = "multi_thread")]
+//!     async fn main() {
+//!         // Data for the first task
+//!         let data1 = vec![1, 2, 3, 4, 5];
+//!
+//!         // Spawning the first `future_for` task
+//!         let handle1 = tokio::spawn(async move {
+//!             FutureLoop::future_for(data1, |num| async move {
+//!                 // Perform an async operation
+//!                 num * 2
+//!             }).await
+//!         });
+//!
+//!         // Data for the second task
+//!         let data2 = vec![6, 7, 8, 9, 10];
+//!
+//!         // Spawning the second `future_for` task
+//!         let handle2 = tokio::spawn(async move {
+//!             FutureLoop::future_for(data2, |num| async move {
+//!                 // Another async operation
+//!                 num * 3
+//!             }).await
+//!         });
+//!
+//!         // Awaiting results from both tasks
+//!         let results1 = handle1.await.expect("Task 1 failed");
+//!         let results2 = handle2.await.expect("Task 2 failed");
+//!
+//!         println!("Results from first task: {:?}", results1);
+//!         println!("Results from second task: {:?}", results2);
+//!     }
+//! ```
+//! Multi Threaded CPU Capacity Test
+//! ```
+//!     use rgwml::loop_utils::FutureLoop;
+//!
+//!     #[tokio::main(flavor = "multi_thread")]
+//!     async fn main() {
+//!         // Start the first test_capacity task and hold its future
+//!         let handle1 = tokio::spawn(async {
+//!             FutureLoop::test_capacity(2500).await;
+//!         });
+//!
+//!         // Start the second test_capacity task with a value of 1000
+//!         let handle2 = tokio::spawn(async {
+//!             FutureLoop::test_capacity(2500).await;
+//!         });
+//!
+//!         // You can await the futures here if you need to get the results or ensure completion
+//!         let _ = handle1.await.expect("Task 1 failed");
+//!         let _ = handle2.await.expect("Task 2 failed");
+//!     }
+//! ```
 //! ## License
 //!
 //! This project is licensed under the MIT License - see the LICENSE file for details.
